b60c71cb3e0fc8fd4bc87c4b06343c6320ee1309
amFarwati <am.farwati@gmail.com>
2023-10-02 13:20:20 +0200

    front +

--- v_0.0/src/components/Inventory.js
+++ v_0.0/src/components/Inventory.js
@@ -5,30 +5,36 @@ import Item from "./InventoryItem";
 import Button from '@mui/joy/Button';
 import Add from '@mui/icons-material/Add';
 
-var listProps=[];
+
 
 function Inventory() {
 
-  const [addProps, setAddProps] = useState(false);
+  const [addItem, setAddItem] = useState(0);
+  const [listItem, setListItem] = useState([]);
 
   useEffect(() => {
+    if (addItem===0){
+      return;
+    }
     const scrollingElement = (document.scrollingElement || document.body);
     scrollingElement.scrollTop = scrollingElement.scrollHeight;
-    listProps.push(/*listProps.length===0?<Item ID ={0}/>:<Item ID ={listProps[listProps.length-1].ID+1}/>); console.log(listProps.length,listProps*/)
-  }, [addProps]);
+    listItem.push(<Item list={listItem} setList={setListItem} id={addItem}/>)
+    setListItem(listItem)
+    
+  }, [addItem]);
 
   return (
     <div className='inventory'>
-      {listProps.map(item => {return <div className='item'>{item}</div>})}
+      {listItem.map((item) => {return <div className='item'>{item}</div>})}
       <Button startDecorator={<Add />} 
               variant="outlined" 
-              onClick={()=>{setAddProps(!addProps)}}
+              onClick={()=>{setAddItem(addItem+1);console.log(addItem)}}
               style={{
                 borderRadius: 30,
                 padding: "18px 36px",
                 fontSize: "20px"
             }}
-              size="lg">Add props</Button>
+              size="lg">Add Item</Button>
     </div>
   )
 }
--- v_0.0/src/components/InventoryItem.js
+++ v_0.0/src/components/InventoryItem.js
@@ -5,8 +5,28 @@ import Divider from '@mui/joy/Divider';
 import Button from '@mui/joy/Button';
 import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
 
-function InventoryItem({props}) {
-    const ID = props.ID;
+var attribute = null
+
+function InventoryItem(props) {
+
+  attribute = {
+    id:props.id,
+    itemId:null,
+    quantity:null,
+    cost:null,
+  }
+  var list = props.list
+
+  function handleDelete(){
+    list.forEach(element => {
+      console.log(element.attribute)
+      if (element.attribute.id === this.attribute.id){
+        let updateList = list.splice(list.indexOf(element),1);
+        props.setList(updateList);
+      };
+    });
+  }
+
   return (
     <div className='item'>
       Prop :
@@ -15,7 +35,7 @@ function InventoryItem({props}) {
       Quantity :
       <Input placeholder="Type in here…" variant="soft" />
       <Divider orientation="vertical" />
-      <Button startDecorator={<RemoveCircleOutlineIcon />} color="danger" high={20} width={20} onClick={function(){}} />
+      <Button startDecorator={<RemoveCircleOutlineIcon />} color="danger" high={20} width={20} onClick={() => handleDelete()} />
     </div>
   )
 }

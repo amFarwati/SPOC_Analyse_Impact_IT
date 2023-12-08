f50703a9d86297c49357fb66465c4c64c730cdff
Larobase <maxime@herry.cc>
2023-12-08 12:51:02 +0100

    debut front

--- v_0.1/front/src/App.js
+++ v_0.1/front/src/App.js
@@ -1,11 +1,12 @@
 import './styles/App.css';
 import Dashboard from "./pages/Dashboard";
+import Opsian from './pages/Opsian';
 
 
 function App() {
   return (
     <div className="App">
-      <Dashboard/>
+      <Opsian/>
     </div>
   );
 }
---
+++ v_0.1/front/src/pages/Opsian.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Fond from "../Assets/Fond.png";
+import '../styles/Opsian.css';
+
+
+function Opsian() {
+    return (
+        <div className="opsian-container">
+          <img src={Fond} className="img-fluid" alt="Fond"></img>
+        </div>
+    );
+}
+
+export default Opsian

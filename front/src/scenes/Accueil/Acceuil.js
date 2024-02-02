b8b118b5635f9b39dd07489965a8b01d0f214c0c
Larobase <maxime@herry.cc>
2024-02-02 16:24:22 +0100

    à propos

--- front/src/scenes/Accueil/Acceuil.js
+++ front/src/scenes/Accueil/Acceuil.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Box, Typography, Slide, Grid } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import backgroundImage from "./fond3.jpg";
+import Maxime from "../../Assets/Maxime.jpg"
+import Arthur from "../../Assets/Arthur.jpg"
+import HBC from "../../Assets/HBC.jpg"
+
 
 const Accueil = () => {
   const theme = useTheme();
@@ -10,26 +14,85 @@ const Accueil = () => {
     <Grid container style={{ minHeight: "100vh" }}>
       <Grid
         item
-        height="30%"
         width="100%"
         sx={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundSize: "cover",
+          backgroundPosition: "center", // Center the background image
           //filter: "blur(8px)",
           zIndex: -1,
         }}
       >
-        <Box sx={{ p: 8, color: theme.palette.primary.main }}>
-          <Slide direction="right" in={true} timeout={2000}>
-            <Typography variant="h2" component="h1" gutterBottom>
+        <Box sx={{ p: 8, color: "#000000", textAlign: "center" }}>
+          <Slide direction="down" in={true} timeout={2000}>
+            <Typography variant="h2" component="h1" gutterBottom style={{ fontSize: "3rem", fontWeight: "bold" }}>
               OPSIAN
             </Typography>
           </Slide>
-          <Slide direction="right" in={true} timeout={3000}>
-            <Typography variant="h6" component="h2" gutterBottom>
-              Votre outil d'analyse d'impact du numérique
+          <Slide direction="down" in={true} timeout={2000}>
+            <Typography variant="h6" component="h2" gutterBottom style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+            OPen Source Impact ANalyzer, est une solution d'analyse d'impact IT qui permet aux utilisateurs d'avoir une idée de leur impact environnemental concernant leur parc informatique.
+
+
             </Typography>
           </Slide>
+          <Box
+            sx={{
+              mt: 4,
+              p: 2,
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              border: "none",
+              borderRadius: "4px"
+            }}
+          >
+            <Typography variant="h4" component="h3" gutterBottom style={{ fontSize: "2rem", fontWeight: "bold" }}>
+              À propos
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+            Nous sommes deux étudiants de l'INSA Lyon, spécialité Télécommunications Services & usages. Pendant nos études, avons pu faire un projet en partenariat avec une entreprise pour concevoir une Sustainable Proof Of Concept ou SPOC.
+            C'est Hugues Benoit-Cattin, notre référent à l'INSA et à La Riche Industrielle, qui nous a présenté les objectifs que devait atteindre notre analyseur d'impact informatique.            </Typography>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}>
+            <Box sx={{ marginRight: "1rem" }}>
+              <img src={Maxime} alt="Image 1" style={{ height: "200px" }} />
+              <Typography sx={{
+              mt: 4,
+              p: 2,
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              border: "none",
+              borderRadius: "4px"
+            }}
+            variant="body2" gutterBottom>
+                Maxime Herry - Développeur
+              </Typography>
+            </Box>
+            <Box sx={{ marginRight: "1rem" }}>
+              <img src={Arthur} alt="Image 2" style={{  height: "200px" }} />
+              <Typography sx={{
+              mt: 4,
+              p: 2,
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              border: "none",
+              borderRadius: "4px"
+            }}
+             variant="body2" gutterBottom>
+                Arthur-Mustapha Farwati - Développeur
+              </Typography>
+            </Box>
+            <Box>
+              <img src={HBC} alt="Image 3" style={{height: "200px" }} />
+              <Typography sx={{
+              mt: 4,
+              p: 2,
+              backgroundColor: "rgba(255, 255, 255, 0.5)",
+              border: "none",
+              borderRadius: "4px"
+            }}
+            variant="body2" gutterBottom>
+                Hugues Benoit-Cattin - Référent
+              </Typography>
+            </Box>
+          </Box>
         </Box>
       </Grid>
     </Grid>

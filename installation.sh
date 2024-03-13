6e44cb8f59aa782a2ec7505fa76ec63403702716
Larobase <maxime.herry@insa-lyon.fr>
2024-03-13 21:06:32 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,15 +1,54 @@
 echo "Installation en cours"
+
+# Mise à jour des paquets
 sudo apt update 
 
-sudo apt install nodejs 
+# Installation de Node.js et npm
+if ! command -v node &> /dev/null
+then
+    echo "Node.js n'est pas installé. Installation en cours..."
+    sudo apt install -y nodejs npm
+else
+    echo "Node.js est déjà installé."
+fi
 
-apt install mysql 
-./Base_de_donnees/MontageBD.sh
+# Installation de MySQL
+if ! command -v mysql &> /dev/null
+then
+    echo "MySQL n'est pas installé. Installation en cours..."
+    sudo apt install -y mysql-server
+else
+    echo "MySQL est déjà installé."
+fi
+
+# Installation de git
+if ! command -v git &> /dev/null
+then
+    echo "Git n'est pas installé. Installation en cours..."
+    sudo apt install -y git
+else
+    echo "Git est déjà installé."
+fi
+
+# Clonage du projet à partir de GitHub
+git clone https://github.com/amFarwati/SPOC_Analyse_Impact_IT
 
 
-cd ./server 
+# Installation des dépendances du projet
+cd GitHub/SPOC_Analyse_Impact_IT
+
+# Configuration de MySQL
+echo "Configuration de MySQL en cours..."
+bash ./Base_de_donnees/MontageBD.sh
+echo "Configuration de MySQL terminée"
+
+# Installation dépendances server
+cd server
 npm install
 npm install redaxios
 
+# Installation dépendances client
 cd ../client
 npm install
+
+echo "Installation terminée"

9bf6a861e567f5dee189c89f2231fd60ea01d6cc
arthur farwati <arthur-mustapha.farwati@insa-lyon.fr>
2024-03-27 08:37:51 +0100

    Update deploy.sh

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,6 +3,6 @@ git checkout production
 
 echo "Deploying files to server..."
 #scp  -i ~/Documents/.ssh/Maxssh server.js package.json package-lock.json numuser@insa-numimpact-01.insa-lyon.fr:server/
-scp  -i /etc/ssh/ssh_host_rsa_key build/* numuser@insa-numimpact-01.insa-lyon.fr:/var/www/opsian.insa-lyon.fr/
+scp  -i /etc/ssh/ssh_host_rsa_key server.js package.json package-lock.json numuser@insa-numimpact-01.insa-lyon.fr:server/
 
 echo "Done!"

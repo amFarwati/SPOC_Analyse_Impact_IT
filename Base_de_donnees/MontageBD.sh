c2914a05cd93a0a1237a9a12feb07f10b047ca5d
Larobase <maxime.herry@insa-lyon.fr>
2024-03-29 10:05:24 +0100

    fix infos montageBD

--- Base_de_donnees/MontageBD.sh
+++ Base_de_donnees/MontageBD.sh
@@ -1,3 +1,3 @@
-echo "Création de la BD"
+echo "Création de la BD. Cette opération mets à jour votre Base de données si vous avez déjà une base de données opsian. Elle écrase les données utilisateurs déjà présentes."
 sudo mysql -u root -p -e "source ./initBD.sql"
 sudo systemctl restart mysql
